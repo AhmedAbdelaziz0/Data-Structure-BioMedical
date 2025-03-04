29ad719f019e3c179fd46a51055ae67813fc85aa
AhmedAbdelaziz0 <ahmed2588547@gmail.com>
2025-03-04 23:33:05 +0200

    Section 3 Answers

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 - `Sheet 1` answers can be found in `Section 1` directory
 - `Sheet 2` answers can be found in `Section 2` directory
-- **`Sheet 3` is uploaded**
+- `Sheet 3` answers can be found in `Section 3`
---
+++ Section_3/main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+
+using namespace std;
+
+class Fraction {
+private:
+  int num;
+  int den;
+
+  int gcd(int a, int b) {
+    if (b == 0)
+      return a;
+    return gcd(b, a % b);
+  }
+
+public:
+  Fraction() {}
+  ~Fraction() {}
+  Fraction(int n, int d) : num(n), den(d) {}
+
+  Fraction operator+(const Fraction &f) {
+    Fraction temp;
+    temp.num = num * f.den + den * f.num;
+    temp.den = den * f.den;
+    return temp;
+  }
+
+  Fraction operator-(const Fraction &f) {
+    Fraction temp;
+    temp.num = num * f.den - den * f.num;
+    temp.den = den * f.den;
+    return temp;
+  }
+
+  Fraction operator*(const Fraction &f) {
+    Fraction temp;
+    temp.num = num * f.num;
+    temp.den = den * f.den;
+    return temp;
+  }
+
+  Fraction operator/(const Fraction &f) {
+    Fraction temp;
+    temp.num = num * f.den;
+    temp.den = den * f.num;
+    return temp;
+  }
+
+  friend istream &operator>>(istream &in, Fraction &f) {
+    char dummy;
+    in >> f.num >> dummy >> f.den;
+    return in;
+  }
+
+  friend ostream &operator<<(ostream &out, const Fraction &f) {
+    out << f.num << "/" << f.den;
+    return out;
+  }
+
+  void simplify() {
+    // gcd impl
+    int g = gcd(num, den);
+    num /= g;
+    den /= g;
+  }
+};
+
+int main() {
+  Fraction f1, f2, res;
+  char op;
+
+  cout << "a op b: \n";
+  cin >> f1 >> op >> f2;
+
+
+  switch (op) {
+  case '+':
+    res = f1 + f2;
+    break;
+  case '-':
+    res = f1 - f2;
+    break;
+  case '/':
+    res = f1 / f2;
+    break;
+  case '*':
+    res = f1 * f2;
+    break;
+  default:
+    cout << "Invalid Operation";
+  }
+  res.simplify();
+  cout << res;
+}
