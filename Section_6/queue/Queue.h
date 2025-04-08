ac60e160ddea93b5ff92f1f1ac8c882011961b29
AhmedAbdelaziz0 <ahmed2588547@gmail.com>
2025-04-08 22:46:31 +0200

    Queue

--- Section_6/queue/Queue.h
+++ Section_6/queue/Queue.h
@@ -16,42 +16,24 @@ private:
 public:
   // constructor that allocates memory
   // set max_size to s which has default value of 5
-  Queue(int s = 5) : size(0), front(0), back(0), max_size(s) { q = new T[s]; }
+  Queue(int s = 5);
 
   // destructor that clears the queue and free memory
-  ~Queue() { delete[] q; }
+  ~Queue();
 
   // for debug
-  void print() const {
-    for (int i = 0; i < size; i ++)
-      std::cout << q[(i + front) % max_size] << ' ';
-    std::cout << std::endl;
-  }
+  void print() const;
 
   // check if the queue is full (how is front relate to back)
-  bool is_full() const { return size == max_size; }
+  bool is_full() const;
 
   // check if the queue is empty
-  bool is_empty() const { return size == 0; }
+  bool is_empty();
 
   // emplace the data at the end of the queue (at back) and advance back index
-  void enqueue(T data) {
-    if (!is_full()) {
-      q[back] = data;
-      back = (back + 1) % max_size;
-      size++;
-    }
-  }
+  void enqueue(T data);
 
   // remove and return the data at the beginning of the queue (at front) and
   // advance front index
-  T dequeue() {
-    if (!is_empty()) {
-      T data = q[front];
-      front = (front + 1) % max_size;
-      size--;
-      return data;
-    }
-    return T();
-  }
+  T dequeue();
 };
