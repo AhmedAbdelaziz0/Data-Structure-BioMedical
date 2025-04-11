0fb98156d354d3804e3a77d0757d53935cda20a8
AhmedAbdelaziz0 <ahmed2588547@gmail.com>
2025-04-11 17:17:16 +0200

    Queue

--- Section_6/queue/Queue.h
+++ Section_6/queue/Queue.h
@@ -3,7 +3,7 @@
 
 template <typename T> class Queue {
 private:
-  T *q;
+  T *arr;
   int max_size;
   int size;
 
@@ -16,24 +16,49 @@ private:
 public:
   // constructor that allocates memory
   // set max_size to s which has default value of 5
-  Queue(int s = 5);
+  Queue(int s = 5) : size(0), front(0), back(0), max_size(s) { arr = new T[s]; }
 
   // destructor that clears the queue and free memory
-  ~Queue();
+  ~Queue() { delete[] arr; }
 
   // for debug
-  void print() const;
+  void print() const {
+    for (int i = 0; i < size; i++)
+      std::cout << arr[(i + front) % max_size] << ' ';
+    std::cout << std::endl;
+  }
 
   // check if the queue is full (how is front relate to back)
-  bool is_full() const;
+  bool is_full() const { return size == max_size; }
 
   // check if the queue is empty
-  bool is_empty();
+  bool is_empty() const { return size == 0; }
 
   // emplace the data at the end of the queue (at back) and advance back index
-  void enqueue(T data);
+  void enqueue(T data) {
+    if (!is_full()) {
+      arr[back] = data;
+      back = (back + 1) % max_size;
+      size++;
+    }
+  }
 
   // remove and return the data at the beginning of the queue (at front) and
   // advance front index
-  T dequeue();
+  T dequeue() {
+    if (!is_empty()) {
+      T data = arr[front];
+      front = (front + 1) % max_size;
+      size--;
+      return data;
+    }
+    return T();
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const Queue& q) {
+    for (int i = 0; i < q.size; i++)
+      os << q.arr[(i + q.front) % q.max_size] << ' ';
+    return os;
+  }
+
 };
